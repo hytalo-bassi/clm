4f211259cda8e2c7a8bbf8e570b968dd8a1eeb23
Hytalo M. Bassi <hytalo-bassi@protonmail.com>
2022-10-08 14:59:13 -0400

    design: utiliza design do Figma para lista de clientes.

--- app/src/main/java/com/example/clm/compose/listacliente/ListaClienteItemView.kt
+++ app/src/main/java/com/example/clm/compose/listacliente/ListaClienteItemView.kt
@@ -1,24 +1,33 @@
 package com.example.clm.compose.listacliente
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Card
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.clm.R
 import com.example.core_db.data.Cliente
 
-fun numberFormat(number: String, template: String, charTemplate: Char): String {
+fun numberFormat(number: String, template: String, charTemplate: Char = '#'): String {
     var formatted = ""
     var numberPosition  = 0
 
@@ -55,6 +64,16 @@ fun ListaClienteItemView(
 ) {
     val selecionado = remember { mutableStateOf(false) }
 
+    val cores: List<Color> = listOf(
+        Color(183, 28, 28),
+        Color(136, 14, 79),
+        Color(74, 20, 140),
+        Color(49, 27, 146),
+        Color(26, 35, 126)
+    )
+
+    val corSelecionada: Color = cores[cores.indices.random()]
+
     fun reverter() {
         selecionado.value = false
         callback(2)
@@ -65,7 +84,7 @@ fun ListaClienteItemView(
         callback(1)
     }
 
-    Card(
+    Row(
         modifier = Modifier
             .padding(horizontal = dimensionResource(id = R.dimen.margem_horizontal_cliente))
             .padding(bottom = dimensionResource(id = R.dimen.margem_chao_cliente))
@@ -90,19 +109,56 @@ fun ListaClienteItemView(
                         selecionar()
                     }
                 )
-            }
+            },
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(Modifier.fillMaxWidth()) {
+        ImagemContato(char = cliente.razaoSocial[0], corSelecionada)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .absolutePadding(left = 8.dp)
+        ) {
             Text(
                 text = cliente.razaoSocial,
-                style = MaterialTheme.typography.subtitle2
+                style = MaterialTheme.typography.h6
             )
 
             Text(
-                text = numberFormat(cliente.clienteCpfCnpj, "###.###.###-##", '#'),
-                style = MaterialTheme.typography.body2
+                text = numberFormat(cliente.clienteCpfCnpj, "###.###.###-##"),
+                style = MaterialTheme.typography.overline
             )
         }
+    }
+
+}
+
+@Composable
+fun ImagemContato(char: Char, background: Color = Color.Black, foregroundColor: Color = Color.White) {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(background, shape = CircleShape)
+            .layout { measurable, constraints ->
+                val placeable = measurable.measure(constraints)
+
+                val currentHeight = placeable.height
+                val currentWidth = placeable.width
+                val newDiameter = maxOf(currentHeight, currentWidth)
+
+                layout(newDiameter, newDiameter) {
+                    placeable.placeRelative(
+                        (newDiameter - currentWidth) / 2,
+                        (newDiameter - currentHeight) / 2
+                    )
+                }
+            }
+    ) {
+        Text(
+            text = char.uppercase(),
+            textAlign = TextAlign.Center,
+            color = foregroundColor,
+            modifier = Modifier.padding(4.dp),
+        )
     }
 }
 
@@ -112,7 +168,7 @@ fun PreviewListaClienteItemView() {
     val cliente = Cliente(
         clienteCpfCnpj = "00000000000",
         razaoSocial = "Razão Social",
-        cep = "00000-000",
+        cep = "00000000",
         uf = "MS",
         cidade = "Campo Grande",
         bairro = "Água Limpa Park",
